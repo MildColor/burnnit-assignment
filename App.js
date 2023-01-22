e1283bf995678e2fd8189fb956ec81774323d3f3
MildColor <96479626+MildColor@users.noreply.github.com>
2023-01-23 00:44:09 +0900

    [FEAT] [subject: navigation 설정] BottomTabs 및 하위 Stack navigation 설정 완료

--- App.js
+++ App.js
@@ -3,20 +3,23 @@ import { StyleSheet, Text, View } from "react-native";
 import Ionicons from "@expo/vector-icons/Ionicons";
 import { FontAwesome5 } from "@expo/vector-icons";
 import styled from "styled-components/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { NavigationContainer } from "@react-navigation/native";
+import RootTabNavigator from "./navigation/RootTabNavigator";
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <Container>
-      <Ionicons name="md-checkmark-circle" size={32} color="green" />
-      <FontAwesome5 name="dumbbell" size={24} color="black" />
-      <StatusBar style="auto" />
-    </Container>
+    <NavigationContainer>
+      <RootTabNavigator />
+    </NavigationContainer>
   );
 }
 
-const Container = styled.View`
-  flex: 1;
-  background-color: pink;
-  align-items: center;
-  justify-content: center;
-`;
+// const Container = styled.View`
+//   flex: 1;
+//   background-color: pink;
+//   align-items: center;
+//   justify-content: center;
+// `;
---
+++ screens/home/HomeMain.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+function HomeMain() {
+  return (
+    <View>
+      <Text>HomeMain</Text>
+    </View>
+  );
+}
+
+export default HomeMain;
